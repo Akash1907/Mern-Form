dd5533555fcda037204f57ada5c5a64a7e735128
Akash <akash.youngster789@gmail.com>
2024-03-10 12:56:29 +0530

    backend cors edit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://mern-form-navy.vercel.app'
+}));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
