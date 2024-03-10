ec6b306d4f4a3227e324eaf183b01a8a267a85e2
Akash <akash.youngster789@gmail.com>
2024-03-10 12:33:53 +0530

    A component is removed

--- frontend/src/Components/AddItem.js
+++ frontend/src/Components/AddItem.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import EditItem from "./EditItem.js";
 import ItemList from "./ItemList.js";
 import "./AddItem.css";
 
