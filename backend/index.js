7b0323461675398d860ff9c7c2aebffed383ad77
Akash <akash.youngster789@gmail.com>
2024-03-10 14:36:34 +0530

    origin configured

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 // app.use(cors());
 app.use(cors({
-  origin: 'https://mern-form-navy.vercel.app'
+  origin: '*'
 }));
+// app.use(cors({
+//   origin: 'https://mern-form-navy.vercel.app'
+// }));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
